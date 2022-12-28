e0802677ed469c7b4bde817beed9df081f7b3cf6
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2022-12-28 10:52:10 +0100

    rewriting code

---
+++ main.py
@@ -0,0 +1,50 @@
+from perceptron import *
+from mnist import MNIST
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+def load_data():
+    data = MNIST(os.getcwd())
+
+    images, labels = data.load_training()
+
+    print(f'loaded training images and labels')
+
+    return images, labels
+
+def show_img(images, labels, index):
+    width = int(np.sqrt(len(images[index])))
+
+    plt.imshow(np.array(images[index]).reshape(width, width), cmap="gray", vmin = 0, vmax = 255)
+
+    plt.title(f'size : {width}x{width}, label : {labels[index]}')
+
+    plt.show()
+
+imgs,labels = load_data()
+
+
+net = init_network([784, 30,10])
+print(f'network initialized')
+
+# goals
+iterations = 100000
+error      = 0.01
+
+i = 0
+err = 1
+
+batch = slice(200,210, 1)
+
+img_batch = np.array(imgs[batch]).T.astype(float)/256
+lbl_batch = labels[batch]
+lbl_img_batch = encode(lbl_batch)
+
+while i <iterations and err >error:
+    i+=1
+    net, err = Update(*net, img_batch, lbl_img_batch, 1)
+    print(f"iteration {i}, error {err}")
+
---
+++ perceptron.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+def encode(model_input):
+    return np.argmax(model_input, axis= 0)
+
+def sigma(x):
+    if x<0.:
+        return np.exp(x)/(1+np.exp(x))
+    else:
+        return 1/(1-np.exp(x))
+
+sgm = np.vectorize(sigma)
+
+def sgm_prime(x):
+    return np.exp(-np.abs(x))/(1+np.exp(-np.abs(x)))
+
+def forward(biases, weights, model_input):
+    layer = model_input
+    for bias, weight in zip(biases, weights):
+        layer = sgm(layer@weight +bias)
+
+    return layer 
+
+def init_network(sizes):
+    biases  = [np.zeros([y,1]) for y in sizes[1:]]
+    weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
+
+    return [biases, weights]
+
+def Update(biases, weights, model_input, label, learning_rate):
+    batch_size = model_input.shape[-1]
+
+    layer = model_input
+    z = []
+    a = []
+    a.append(layer)
+
+    for bias, weight in zip(biases, weights):
+          
+        zi = weight@layer + bias
+        z.append(zi)
+        layer = sgm(zi)
+        a.append(layer)
+
+    error = np.linalg.norm(a[-1] - label)/batch_size
+
+    dlt = (a[-1]-label)*sgm_prime(z[-1]) /batch_size
+
+    new_biases, new_weights = biases, weights
+
+    new_biases[-1][:,0] = new_biases[-1][:,0] - learning_rate*dlt.sum(axis=1) 
+    new_weights[-1] = new_biases[-1] - learning_rate*(dlt@a[-2].T)
+
+    #for la
+    for layer_index in range(len(weights)-1, 0, -1):
+
+        dlt = weights[layer_index].T@dlt * sgm_prime(z[layer_index-1])
+
+
+
+        new_biases[layer_index-1][:,0] = new_biases[layer_index-1][:,0] - learning_rate*dlt.sum(axis=1)
+        new_weights[layer_index-1] = new_weights[layer_index-1] - learning_rate* (dlt@a[layer_index-1].T)
+
+
+    return [new_biases, new_weights], error
+        
